28e9b2e56e6f513659047054d5722efd12631a02
chrisfrohmaier <c.frohmaier@soton.ac.uk>
2015-10-25 12:54:26 +0000

    Amazon AWS db added

--- runCloudDetect.py
+++ runCloudDetect.py
@@ -24,7 +24,7 @@ for i in range(0,5):
 	print 'Altitude: ', bmp.read_altitude()
 	cur.execute("INSERT INTO nightlog (ir_temp,amb_temp,pressure,die_temp,altitude,voltage) VALUES (%s,%s,%s,%s,%s,%s) RETURNING imageid;", (float(tmp.readObjTempC()),float(bmp.read_temperature()),float(bmp.read_pressure()),float(tmp.readDieTempC()),float(bmp.read_altitude()),float(tmp.readVoltage()),))
 	ir_id=cur.fetchone()
-	print ir_id
+	print int(ir_id[0])
 	sleep(10)
 
 	print '-----'
