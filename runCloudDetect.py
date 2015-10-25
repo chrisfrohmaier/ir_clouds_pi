3e396f868754d5759c05819dbf59fc0310412bf6
chrisfrohmaier <c.frohmaier@soton.ac.uk>
2015-10-25 13:04:11 +0000

    IR Camera

--- runCloudDetect.py
+++ runCloudDetect.py
@@ -8,6 +8,9 @@ from time import sleep  # pull in the sleep function from time module
 from BMP085 import BMP085
 from TMP007 import TMP007
 import psycopg2
+import picamera
+
+camera = picamera.PiCamera()
 
 conn = psycopg2.connect(host='frohmaierdb.c8d7umbjrqmj.us-west-2.rds.amazonaws.com', port=5432, user='cf5g09', password='SotonRoof2015!!', database='B46Roof')
 cur=conn.cursor()
@@ -25,6 +28,11 @@ for i in range(0,5):
 	cur.execute("INSERT INTO nightlog (ir_temp,amb_temp,pressure,die_temp,altitude,voltage) VALUES (%s,%s,%s,%s,%s,%s) RETURNING imageid;", (float(tmp.readObjTempC()),float(bmp.read_temperature()),float(bmp.read_pressure()),float(tmp.readDieTempC()),float(bmp.read_altitude()),float(tmp.readVoltage()),))
 	ir_id=cur.fetchone()
 	print int(ir_id[0])
+	conn.commit()
+	camera.capture('/home/pi/ir_clouds_pi/night_images/IR_Image_'+str(ir_id)+'.jpg')
+	
+
+
 	sleep(10)
 
 	print '-----'
